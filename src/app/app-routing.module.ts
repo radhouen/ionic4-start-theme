d80e233f80ba38d6f19f16b5cf1ab83253aa4346
Radhouen Assakra <radhouen.assakra@target-energysolutions.com>
2021-02-19 21:19:53 +0100

    upgrade to angular 9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,22 +2,22 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './pages/login/login.module#LoginPageModule' },
-  { path: 'register', loadChildren: './pages/register/register.module#RegisterPageModule' },
-  { path: 'about', loadChildren: './pages/about/about.module#AboutPageModule' },
-  { path: 'settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
-  { path: 'edit-profile', loadChildren: './pages/edit-profile/edit-profile.module#EditProfilePageModule' },
-  { path: 'home-results', loadChildren: './pages/home-results/home-results.module#HomeResultsPageModule' },
-  { path: 'essence', loadChildren: './pages/essence/essence.module#EssencePageModule' },
-  { path: 'diesel', loadChildren: './pages/diesel/diesel.module#DieselPageModule' },
-  { path: 'marins', loadChildren: './pages/marins/marins.module#MarinsPageModule' },
-  { path: 'httemps', loadChildren: './pages/httemps/httemps.module#HttempsPageModule' },
-  { path: 'hmgaz', loadChildren: './pages/hmgaz/hmgaz.module#HmgazPageModule' },
-  { path: 'htransmissions', loadChildren: './pages/htransmissions/htransmissions.module#HtransmissionsPageModule' },
-  { path: 'caloporteurs', loadChildren: './pages/caloporteurs/caloporteurs.module#CaloporteursPageModule' },
-  { path: 'hydrauliques', loadChildren: './pages/hydrauliques/hydrauliques.module#HydrauliquesPageModule' },
-  { path: 'graisses', loadChildren: './pages/graisses/graisses.module#GraissesPageModule' },
-  { path: 'viewdata', loadChildren: './pages/viewdata/viewdata.module#ViewdataPageModule' },
+  { path: '', loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule) },
+  { path: 'register', loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterPageModule) },
+  { path: 'about', loadChildren: () => import('./pages/about/about.module').then(m => m.AboutPageModule) },
+  { path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsPageModule) },
+  { path: 'edit-profile', loadChildren: () => import('./pages/edit-profile/edit-profile.module').then(m => m.EditProfilePageModule) },
+  { path: 'home-results', loadChildren: () => import('./pages/home-results/home-results.module').then(m => m.HomeResultsPageModule) },
+  { path: 'essence', loadChildren: () => import('./pages/essence/essence.module').then(m => m.EssencePageModule) },
+  { path: 'diesel', loadChildren: () => import('./pages/diesel/diesel.module').then(m => m.DieselPageModule) },
+  { path: 'marins', loadChildren: () => import('./pages/marins/marins.module').then(m => m.MarinsPageModule) },
+  { path: 'httemps', loadChildren: () => import('./pages/httemps/httemps.module').then(m => m.HttempsPageModule) },
+  { path: 'hmgaz', loadChildren: () => import('./pages/hmgaz/hmgaz.module').then(m => m.HmgazPageModule) },
+  { path: 'htransmissions', loadChildren: () => import('./pages/htransmissions/htransmissions.module').then(m => m.HtransmissionsPageModule) },
+  { path: 'caloporteurs', loadChildren: () => import('./pages/caloporteurs/caloporteurs.module').then(m => m.CaloporteursPageModule) },
+  { path: 'hydrauliques', loadChildren: () => import('./pages/hydrauliques/hydrauliques.module').then(m => m.HydrauliquesPageModule) },
+  { path: 'graisses', loadChildren: () => import('./pages/graisses/graisses.module').then(m => m.GraissesPageModule) },
+  { path: 'viewdata', loadChildren: () => import('./pages/viewdata/viewdata.module').then(m => m.ViewdataPageModule) },
 ];
 
 @NgModule({
